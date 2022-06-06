dbb82c67aa1b2cc35c73e8800d9b7a882d75d4c0
AdityaBahl <adityabahl12345@gmail.com>
2022-06-06 21:58:35 +0530

    second file

---
+++ 2.r
@@ -0,0 +1,18 @@
+#vectors
+#c is used in vector, meaning combine
+a1=c(1,2,3,4,5,6)
+a1
+a2=c("aditya","bahl","is","the","greatest")
+a2
+a3=c(12,5,6,13)
+a3*2
+a4=c(1,2,4,8)
+a4^2
+sqrt(a4)
+sqrt(a4^2)
+a5=1:10
+a5
+length(a5)
+a6=31:40
+a6
+a5+a6
