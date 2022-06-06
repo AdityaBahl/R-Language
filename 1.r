03d9afee0225a378cc1a3c8a60c5eae4600e9268
AdityaBahl <adityabahl12345@gmail.com>
2022-06-06 21:58:20 +0530

    first file

---
+++ 1.r
@@ -0,0 +1,34 @@
+#datatypes etc
+3+7
+3-2
+8+2*10
+x=10
+x+100
+y=10
+y<-20
+y+x
+assign("a",8)
+a+5
+rm(a)
+a+5
+class(x)
+class("hello")
+is.numeric(x)
+a=as.Date("16-05-2022")
+class(a)
+#cant assign date like, a="12-05-22"
+
+#to find out length
+a="Aditya Bahl"
+nchar(a)
+#if,else,else if
+x=15
+y=10
+if(x==y){
+  a=10
+}else if(x>5){
+  a=a+100
+}else{
+  a=20
+}
+a
